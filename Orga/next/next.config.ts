74d03493da53e6b7685eaf164532d088577bb58e
zzam <73723224+zzam-dev@users.noreply.github.com>
2025-07-17 01:57:56 +0300

    Update next.config.ts

--- Orga/next/next.config.ts
+++ Orga/next/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // enables static HTML export
+  basePath: '/P2.0.0a', // the repo name (case-sensitive!)
+  assetPrefix: '/P2.0.0a', // ensures styles & scripts load properly
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
